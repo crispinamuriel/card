48c24a4c91c53813bb31cdfab3ebfda3c236747a
crispinamuriel <crispinamuriel@gmail.com>
2024-01-08 21:45:06 -0500

    rename item var

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,20 +45,20 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen flex-col items-center p-24">
-      {cards.map(item => (
-        <div key={item.card_id} className="flex flex-col max-w-sm w-full lg:max-w-full lg:flex mb-10 max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70 hover:bg-slate-950">
+      {cards.map(card => (
+        <div key={card.card_id} className="flex flex-col max-w-sm w-full lg:max-w-full lg:flex mb-10 max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70 hover:bg-slate-950">
           <span className="max-w-fit mb-2 bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">
-            {item.type}
+            {card.type}
           </span>
-          <h3 className="text-2xl font-semibold text-green-600 mb-4">{item.headline}</h3>
-          <div dangerouslySetInnerHTML={{ __html: item.body }} />
-          {item.type == 'audio' && item?.audio !== null && item?.audio !== undefined ? (
+          <h3 className="text-2xl font-semibold text-green-600 mb-4">{card.headline}</h3>
+          <div dangerouslySetInnerHTML={{ __html: card.body }} />
+          {card.type == 'audio' && card?.audio !== null && card?.audio !== undefined ? (
             <audio controls>
-              <source src={item.audio} type={'audio/mpeg'} />
+              <source src={card.audio} type={'audio/mpeg'} />
               Your browser does not support the audio element.
             </audio>) : ''}
-          {item.type == 'question' && item.answerOptions !== null && item?.answerOptions !== undefined ?
-            (<QuizCommponent answerOptions={item.answerOptions} />) : ''}
+          {card.type == 'question' && card.answerOptions !== null && card?.answerOptions !== undefined ?
+            (<QuizCommponent answerOptions={card.answerOptions} />) : ''}
         </div>
       ))}
     </div>
